8c05c967a9caecaa9a6f116c0849ee9bdc669d05
Ebanflo42 <eben.kadile24@gmail.com>
2022-07-08 23:03:46 +0200

    working on sweet animation

--- resources/scripts/displayZipfGeometry.js
+++ resources/scripts/displayZipfGeometry.js
@@ -56,13 +56,14 @@ freq1dCamera.position.z = -2
 freq1dCamera.lookAt(new THREE.Vector3(0, 0, 0))
 
 var freq1dGeometry = new THREE.BufferGeometry();
-freq1dGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(3 * linePoints), 3));
-let posArray = freq1dGeometry.getAttribute('position').array;
+freq1dGeometry.setAttribute('position',
+							new THREE.BufferAttribute(new Float32Array(3 * linePoints), 3));
+let freq1dPosArray = freq1dGeometry.getAttribute('position').array;
 for(let i = 0; i < linePoints; i++) {
 	let x = (i - 0.5*(linePoints - 1))/(linePoints - 1);
-	posArray[3*i] = x;
-	posArray[3*i + 1] = 0.45*Math.sin(4*Math.PI*x);
-	posArray[3*i + 2] = 0;
+	freq1dPosArray[3*i] = x;
+	freq1dPosArray[3*i + 1] = 0.45*Math.sin(4*Math.PI*x);
+	freq1dPosArray[3*i + 2] = 0;
 }
 
 var lineMaterial = new THREE.ShaderMaterial({
@@ -80,10 +81,132 @@ freq1dScene.add(freq1dLine);
 var inpFreq = document.getElementById('inpFreq');
 inpFreq.oninput = function () {
 	let posAttr = freq1dGeometry.getAttribute('position');
-	let posArray = posAttr.array;
+	let freq1dPosArray = posAttr.array;
 	for(let i = 0; i < linePoints; i++) {
 		let x = (i - 0.5*(linePoints - 1))/(linePoints - 1);
-		posArray[3*i + 1] = 0.45*Math.sin(2*this.value*Math.PI*x);
+		freq1dPosArray[3*i + 1] = 0.45*Math.sin(2*this.value*Math.PI*x);
+	}
+	posAttr.needsUpdate = true;
+}
+
+
+// High Dimensional Projection ////////////////////////////////////////////////
+
+
+var axis1 = new Array();
+for(let i = 0; i < 300; i++) {
+	axis1.push(Math.random());
+}
+var norm = 0;
+for(let i = 0; i < 300; i++) {
+	norm += axis1[i]*axis1[i];
+}
+for(let i = 0; i < 300; i++) {
+	axis1[i] /= Math.sqrt(norm);
+}
+// super dumb way to create orthogonal vectors
+var axis2 = new Array();
+for(let i = 0; i < 150; i++) {
+	axis2.push(axis1[2*i + 1]);
+	axis2.push(-axis1[2*i]);
+}
+var axis3 = new Array();
+for(let i = 0; i < 100; i++) {
+	axis3.push(axis1[3*i + 2]*axis2[3*i + 3] - axis1[3*i + 3]*axis2[3*i + 2]);
+	axis3.push(axis1[3*i + 1]*axis2[3*i + 3] - axis1[3*i + 3]*axis2[3*i + 1]);
+	axis3.push(axis1[3*i + 1]*axis2[3*i + 2] - axis1[3*i + 2]*axis2[3*i + 1]);
+}
+
+var phases = new Array();
+for(let i = 0; i < 300; i++) {
+	phases.push(Math.PI*Math.random());
+}
+var exponent = -1;
+
+var highdPoints = new Array();
+for(let i = 0; i < linePoints + 1; i++) {
+	let point = new Array();
+	for(let j = 0; j < 300; j++) {
+		let wav = Math.sin(2*Math.PI*i/linePoints + phases[j]);
+		let amp = Math.pow(j + 1, exponent);
+		point.push(amp*wav);
+	}
+	highdPoints.push(point)
+}
+
+var highdCanvas = document.getElementById('highdCanvas');
+var highdCamera = new THREE.PerspectiveCamera(60, highdCanvas.clientWidth / highdCanvas.clientHeight, 1, 20);
+var cameraControl = new THREE.OrbitControls(highdCamera, highdCanvas);
+cameraControl.rotateSpeed = 0.0;
+highdCamera.position.set(0, 0, 5);
+highdCamera.lookAt(new THREE.Vector3(0, 0, 0));
+
+var highdGeometry = new THREE.BufferGeometry();
+highdGeometry.setAttribute('position',
+						    new THREE.BufferAttribute(new Float32Array(3 * (linePoints + 1)), 3));
+var highdPosArray = highdGeometry.getAttribute('position').array;
+for(let i = 0; i < linePoints + 1; i++) {
+	highdPosArray[3*i] = 0;
+	highdPosArray[3*i + 1] = 0;
+	highdPosArray[3*i + 2] = 0;
+	for(let j = 0; j < 300; j++) {
+		highdPosArray[3*i] += axis1[j]*highdPoints[i][j];
+		highdPosArray[3*i + 1] += axis2[j]*highdPoints[i][j];
+		highdPosArray[3*i + 2] += axis3[j]*highdPoints[i][j];
+		console.log(highdPosArray[3*i + 2])
+	}
+}
+
+let vertCol = new Float32Array(3*(linePoints + 1));
+for (let i = 0; i < linePoints + 1; i++) {
+	vertCol[3*i] = Math.sin(2*Math.PI*i/linePoints);
+	vertCol[3*i + 1] = Math.sin(2*Math.PI*i/linePoints + 0.333*Math.PI);
+	vertCol[3*i + 2] = Math.sin(2*Math.PI*i/linePoints + 0.66*Math.PI);
+}
+highdGeometry.setAttribute('vertCol', new THREE.BufferAttribute(vertCol, 3));
+highdGeometry.setDrawRange(0, linePoints + 1);
+
+var highdMaterial = new THREE.ShaderMaterial({
+	vertexShader: document.getElementById('highd_vs').textContent,
+	fragmentShader: document.getElementById('highd_fs').textContent,
+	uniforms: {p: {type: 'f', value: exponent}},
+	depthWrite: false,
+	depthTest: false,
+	linewidth: 2,
+	transparent: true
+});
+
+var highdScene = new THREE.Scene();
+var highdLine = new THREE.Line(highdGeometry, highdMaterial);
+highdScene.add(highdLine);
+
+var highdRenderer = new THREE.WebGLRenderer({ canvas: highdCanvas, antialias: true, alpha: false });
+highdRenderer.setSize(highdCanvas.clientWidth, highdCanvas.clientHeight);
+
+var expInput = document.getElementById("expInput");
+expInput.oninput = function() {
+
+	exponent = -this.value/20;
+
+	for(let i = 0; i < linePoints; i++) {
+		for(let j = 0; j < 300; j++) {
+			let wav = Math.sin(2*Math.PI*i/linePoints + phases[j]);
+			let amp = Math.pow(j + 1, exponent);
+			highdPoints[i][j] = amp*wav;
+		}
+	}
+
+	let posAttr = highdGeometry.getAttribute('position');
+	let posArray = posAttr.array;
+	for(let i = 0; i < linePoints + 1; i++) {
+		posArray[3*i] = 0;
+		posArray[3*i + 1] = 0;
+		posArray[3*i + 2] = 0;
+		for(let j = 0; j < 300; j++) {
+			posArray[3*i] += axis1[j]*highdPoints[i][j];
+			posArray[3*i + 1] += axis2[j]*highdPoints[i][j];
+			posArray[3*i + 2] += axis3[j]*highdPoints[i][j];
+		}
 	}
 	posAttr.needsUpdate = true;
 }
@@ -109,11 +232,21 @@ function render() {
   	    freq1dRes = new THREE.Vector2(freq1dCanvas.clientWidth, freq1dCanvas.clientHeight);
     }
 
+    if(highdCanvas.width !== highdCanvas.clientWidth || highdCanvas.height !== highdCanvas.clientHeight) {
+		highdRenderer.setSize(highdCanvas.clientWidth, highdCanvas.clientHeight, false);
+		highdCamera.aspect = highdCanvas.clientWidth/highdCanvas.clientHeight;
+		highdCamera.updateProjectionMatrix();
+		highdRes = new THREE.Vector2(highdCanvas.clientWidth, highdCanvas.clientHeight);
+  }
+
     freq2dShader.uniforms['res'].value = freq2dRes;
     freq2dShader.uniforms['freqs'].value = freqs2d;
 
+	highdMaterial.uniforms['p'].value = exponent;
+
     freq2dRenderer.render(freq2dScene, freq2dCamera);
     freq1dRenderer.render(freq1dScene, freq1dCamera);
+	highdRenderer.render(highdScene, highdCamera);
 
 }
 
